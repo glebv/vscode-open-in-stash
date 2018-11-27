7b9042b8cc4a5eaa37b7d8213fab37cc3f862989
Gleb Vinnikov <gleb.vinnikov@gmail.com>
2018-11-27 11:53:51 +0200

    added initial functionality

---
+++ README.md
@@ -0,0 +1,63 @@
+![vscode-open-in-stash]("Open in Stash Atlassian")  
+
+**Supports :** Stash Atlassian
+
+> Extension for Visual Studio Code which can be used to jump to a source code line in Stash Atlassian
+
+
+## Install
+
+**Tested with VsCode 1.29.1**  
+
+Press <kbd>F1</kbd> and narrow down the list commands by typing `extension`. Pick `Extensions: Install Extension`.
+
+![installation](screenshots/install.png?raw=true "installation")
+
+Simply pick the `Open in Stash` extension from the list
+
+## Install Manual
+
+### Mac & Linux
+
+```sh
+cd $HOME/.vscode/extensions
+git clone https://github.com/glebv/vscode-open-in-stash.git
+cd vscode-open-in-stash
+yarn install
+```
+
+### Windows
+
+```sh
+cd %USERPROFILE%\.vscode\extensions
+git clone https://github.com/glebv/vscode-open-in-stash.git
+cd vscode-open-in-stash
+yarn install
+```
+
+## Usage
+
+### Command
+
+Press <kbd>F1</kbd> and type `Open in Stash`.
+
+![open](screenshots/open-in-stash.png?raw=true "Open function")
+
+
+### Configure custom github domain
+
+Add following line into workspace settings;
+
+```js
+{
+  "openInStash.stashDomain": "your custom github domain here",
+}
+```
+
+**Enjoy!**
+
+#### TODO
+
+* tests
+* icon
+* publish to vscode extension markeplace 
---
+++ src/extension.ts
@@ -0,0 +1,81 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+const gitUrlParse = require('git-url-parse');
+const gitConfig = require('gitconfiglocal');
+const findParentDir = require('find-parent-dir');
+const gitBranch = require('git-branch');
+const path = require('path');
+const open = require('open');
+// import pify from 'pify';
+
+const stashDomain = vscode.workspace.getConfiguration('openInStash').get('stashDomain', '');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vscode-open-in-stash" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    context.subscriptions.push(vscode.commands.registerCommand('extension.openInStash', openInStashPovider));
+    // context.subscriptions.push(vscode.commands.registerCommand('extension.openInStashPR', openInStashPR));
+}
+
+// const openInStashPR = (args: any) => {
+
+// };
+
+const openInStashPovider = (args: any) => {
+    const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+    if( !stashDomain ) {
+        vscode.window.showErrorMessage('Stash domain is not specified, please set it in workspace configuration');
+    }
+
+    if (editor) {
+        const fileFsPath: string = editor.document.fileName;
+
+        var repoDir = findParentDir.sync(fileFsPath, '.git');
+        if (!repoDir) {
+            throw Error('Cant locate .git repository for this file');
+        }
+        
+        gitConfig(repoDir, (err: any, config: any) => {
+            if (err || !config) {
+                throw Error('Unable get git config');
+            }
+            const gitUrl: string = gitUrlParse(config.remote.origin.url);
+            const branch = gitBranch.sync(repoDir);
+
+            let formattedFilePath: string = path.relative(repoDir, fileFsPath).replace(/\\/g, '/');
+            formattedFilePath = formattedFilePath.replace(/\s{1}/g, '%20');
+            const filePath: string = repoDir !== fileFsPath ? '/' + formattedFilePath : '';
+
+            const url = webUrl(stashDomain, gitUrl, branch, filePath);
+            open(url);
+        });
+
+    }
+
+};
+
+const webUrl = (baseUrl:string = stashDomain, gitUrl: any, branch: string, filePath: string):string => {
+    return `${baseUrl}/projects/${gitUrl.owner}/repos/${gitUrl.name}/browse` 
+        + (filePath ? `${filePath}` : '') 
+        + (branch ? `?at=refs/heads/${branch}` : '');
+};
+
+const prUrl = () => {
+    //https://stash.odesk.com/projects/UC/repos/ui-packages/pull-requests/96/diff
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
